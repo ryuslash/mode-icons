96c183a2792b3c1ac76befae9dcc159c56adaac3
Tom Willemsen <tom@ryuslash.org>
2012-11-10 23:21:46 +0100

    Add version header

--- mode-icons.el
+++ mode-icons.el
@@ -4,6 +4,7 @@
 
 ;; Author: Tom Willemsen <thomas@aethon.nl>
 ;; Keywords: multimedia
+;; Version: 0.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
