1d0f1b529fe86ff0d90e3aedd450676cad8f8e2f
Tom Willemse <tom@ryuslash.org>
2016-01-21 22:07:28 +0100

    Add commentary section to package

--- mode-icons.el
+++ mode-icons.el
@@ -22,7 +22,33 @@
 
 ;;; Commentary:
 
+;; This package provides a globalized minor mode that replaces the
+;; major mode name in your mode-line and places like Ibuffer with an
+;; icon.  Currently the following programming modes are supported,
+;; along with some other modes:
 ;;
+;; - CSS
+;; - Coffee
+;; - Emacs-Lisp
+;; - HTML
+;; - Haml
+;; - JavaScript
+;; - Lisp
+;; - nXML
+;; - PHP
+;; - Python
+;; - Ruby
+;; - Sass/Scss
+;; - Scheme
+;; - Shell-script
+;; - Slim
+;; - Snippet
+;; - Web
+;; - Yaml
+;;
+;; To enable this minor mode add the following line to your init file:
+;;
+;;     (mode-icons-mode)
 
 ;;; Code:
 
